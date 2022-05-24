076be3acb1e05d6150e6d3f71d7091fadc991591
Max Kofler <kofler.max.dev@gmail.com>
2022-05-24 13:27:43 -0700

    Added a_update() a_install() and setRootDir() to Leafcore

--- pyleafcore.py
+++ pyleafcore.py
@@ -1,4 +1,4 @@
-from ctypes import cdll
+from ctypes import *
 
 cleaf = cdll.LoadLibrary("libcleaf.so")
 
@@ -7,7 +7,26 @@ cleaf.cleaf_init(17)
 class Leafcore():
 
 	def __init__(self):
-		self.obj = cleaf.cleaf_new()
+		self.obj = cleaf.cleafcore_new()
 
 	def __del__(self):
-		cleaf.cleaf_delete(self.obj)
+		cleaf.cleafcore_delete(self.obj)
+
+	def setRootDir(self, rootDir):
+		cleaf.cleafconfig_setRootDir(bytes(rootDir, encoding='utf-8'))
+
+	def a_update(self):
+		cleaf.cleafcore_a_update(self.obj)
+
+	def a_install(self, packages):
+		arr = (c_char_p * len(packages))()
+
+		for i in range(0, len(packages)):
+			print("Adding argument " + str(i) + ": " + packages[i])
+			c_str = (packages[i]).encode('utf-8')
+			arr[i] = c_char_p(c_str)
+
+		cleaf.cleafcore_readDefaultPackageList(self.obj)
+		cleaf.cleafcore_a_install(self.obj, len(packages), arr)
+
+
--- test.py
+++ test.py
@@ -1,3 +1,10 @@
+from re import S
 from pyleafcore import *
 
 leafcore = Leafcore()
+
+leafcore.setRootDir("./root")
+leafcore.a_update()
+
+packages = ["base"]
+leafcore.a_install(packages)
