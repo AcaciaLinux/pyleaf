aa6e00b8cd41c2a8518545ed147fec7fdcdb7542
zimsneexh <zimsneexh@gmail.com>
2022-05-29 21:56:26 +0200

    Fix weird indentation in pyleafcore.py

--- pyleafcore.py
+++ pyleafcore.py
@@ -1,34 +1,37 @@
 from ctypes import *
 
 class Leafcore():
-
-	def __init__(self):
+    def __init__(self):
         self.cleaf = cdll.LoadLibrary("libcleaf.so")
-        self.cleaf.cleaf_setLogLevel(0)
-        self.obj = self.cleaf.cleafcore_new()
         
+        # init arg is Verbosity?
+        self.cleaf.cleaf_init(0)
+        
+        self.leafcore = self.cleaf.cleafcore_new()
+
+
     def setVerbosity(self, verbosity):
         # Verbosity from 0 (normal) - 3 (ultraverbose)
         self.cleaf.cleaf_setLogLevel(verbosity)
 
-	def __del__(self):
-		self.cleaf.cleafcore_delete(self.obj)
+    def __del__(self):
+	    self.cleaf.cleafcore_delete(self.leafcore)
 
-	def setRootDir(self, rootDir):
-		self.cleaf.cleafconfig_setRootDir(bytes(rootDir, encoding='utf-8'))
+    def setRootDir(self, rootDir):
+        self.cleaf.cleafconfig_setRootDir(bytes(rootDir, encoding='utf-8'))
 
-	def a_update(self):
-		self.cleaf.cleafcore_a_update(self.obj)
+    def a_update(self):
+	    self.cleaf.cleafcore_a_update(self.leafcore)
 
-	def a_install(self, packages):
-		arr = (c_char_p * len(packages))()
+    def a_install(self, packages):
+        arr = (c_char_p * len(packages))()
 
-		for i in range(0, len(packages)):
-			print("Adding argument " + str(i) + ": " + packages[i])
-			c_str = (packages[i]).encode('utf-8')
-			arr[i] = c_char_p(c_str)
+        for i in range(0, len(packages)):
+            print("Adding argument " + str(i) + ": " + packages[i])
+            c_str = (packages[i]).encode('utf-8')
+            arr[i] = c_char_p(c_str)
 
-		self.cleaf.cleafcore_readDefaultPackageList(self.obj)
-		self.cleaf.cleafcore_a_install(self.obj, len(packages), arr)
+        self.cleaf.cleafcore_readDefaultPackageList(self.leafcore)
+        self.cleaf.cleafcore_a_install(self.leafcore, len(packages), arr)
 
 
