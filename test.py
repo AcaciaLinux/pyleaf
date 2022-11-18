5c54ab2712b395b7ad118cb0090bf640dfb5abfc
Max Kofler <kofler.max.dev@gmail.com>
2022-11-17 22:07:37 -0800

    Added more tests to test.py

--- test.py
+++ test.py
@@ -1,12 +1,15 @@
+#!python3
+
 from re import S
 from pyleafcore import *
 
 leafcore = Leafcore()
 
 leafcore.setRootDir("./root")
+leafcore.setBoolConfig(LeafConfig_bool.CONFIG_NOASK, True)
 leafcore.a_update()
 
-packages = ["crosstools"]
+packages = ["base"]
 res = leafcore.a_install(packages)
 
 if (res != 0):
