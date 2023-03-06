46de78cce4dc1ff9195b40a45237142f17d64089
Max Kofler <kofler.max.dev@gmail.com>
2023-03-06 15:21:26 +0100

    test.py: Update for LeafException

--- src/test.py
+++ src/test.py
@@ -11,24 +11,16 @@ leafcore.setBoolConfig(LeafConfig_bool.CONFIG_NOPROGRESS, True)
 leafcore.setBoolConfig(LeafConfig_bool.CONFIG_FORCE, False)
 leafcore.setStringConfig(LeafConfig_string.CONFIG_PKGLISTURL, "https://api.acacialinux.org/?get=packagelist")
 leafcore.setStringConfig(LeafConfig_string.CONFIG_DOWNLOADCACHE, "./leafcache/")
-if (not leafcore.a_update() == 0):
-	print("Failed to get package list (code {}): {}".format(leafcore.getLastErrorCode(), leafcore.getLastErrorString()))
-	exit(-1)
+leafcore.a_update()
 
 packages = ["glibc", "readline", "ncurses"]
-res = leafcore.a_install(packages)
-
-if (res != 0):
-	print("Leafcore error code: {}".format(res))
+leafcore.a_install(packages)
  
 Upackages = []
-res = leafcore.a_upgrade(Upackages)
+leafcore.a_upgrade(Upackages)
 
 Rpackages = ["readline"]
 leafcore.a_remove(Rpackages)
-
-if (res != 0):
-	print("Leafcore error code: {}".format(res))
 
 print("")
 print("----- LEAF DEBUG LOG -----")
